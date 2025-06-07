b2d2a620ad04058686e3dbdf4050be3caf48ab4b
takyo41 <59023352+takyo41@users.noreply.github.com>
2025-06-07 16:01:33 +0200

    Update and rename exercice4.sql to 2024_BDD_BRAY_HUGO_EX4.sql

--- modelisation-1/2024_BDD_BRAY_HUGO_EX4.sql
+++ modelisation-1/2024_BDD_BRAY_HUGO_EX4.sql
@@ -7,8 +7,14 @@ SELECT COUNT(*) AS TotalReservations
 FROM Reservation
 WHERE DateDebut BETWEEN '2025-05-25' AND '2025-05-30';
 
+-- Le count(*) permet de compter toutes les colones sans exception
+-- Le as nomme le resultat du count(*)
+
+
 
 --Question 2 : 
 SELECT COUNT(DISTINCT R.IdUtilisateur) AS NombreUtilisateurs
 FROM Reservation R
 JOIN Materiel M ON R.IdMateriel = M.IdMateriel;
+
+-- Le DISTINCT permet d'enlever les doublons
